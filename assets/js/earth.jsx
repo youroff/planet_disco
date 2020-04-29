885eb23cf6bb6a1700d26be85183ba10bf7e70c6
Ivan Youroff <ivan.youroff@gmail.com>
2020-04-29 21:55:25 +0200

    more cleanup

--- assets/js/earth.jsx
+++ assets/js/earth.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo } from 'react'
+import React, { useRef, useMemo, Suspense } from 'react'
 import * as THREE from 'three/src/Three'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
@@ -14,16 +14,16 @@ const CITIES = gql`{
 
 export function Earth(props) {
   const { loading, error, data } = useQuery(CITIES);
-  console.log(data)
-  const mesh = useRef()
-  const texture = useMemo(() => new THREE.TextureLoader().load("/images/8081_earthspec10k.gif"), [])
+  const texture = useLoader(THREE.TextureLoader, "/images/8081_earthspec10k.gif")
   
   texture.magFilter = THREE.NearestFilter
 
   return (
-    <mesh {...props} ref={mesh}>
-      <sphereGeometry attach="geometry" args={[1, 32, 32]} />
-      <meshLambertMaterial attach="material" map={texture} />
-    </mesh>
+    <Suspense fallback={null}>
+      <mesh {...props}>
+        <sphereGeometry attach="geometry" args={[1, 32, 32]} />
+        <meshLambertMaterial attach="material" map={texture} />
+      </mesh>
+    </Suspense>
   )
 }
