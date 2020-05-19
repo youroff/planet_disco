3a15a65bf7ebf2f7372d0e7eb20c43437fa4701f
Sea94 <ekaterina.svikhnushina@epfl.ch>
2020-05-19 21:28:40 +0200

    genre_view size and color modifications

--- assets/js/planet_disco/genres_view/box.jsx
+++ assets/js/planet_disco/genres_view/box.jsx
@@ -1,16 +1,16 @@
 import React, { useRef, useState } from 'react'
-import { useFrame } from 'react-three-fiber'
+import { useFrame, applyProps } from 'react-three-fiber'
 
 export default (props) => {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
-  
+
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   
   return (
     <mesh
@@ -20,8 +20,26 @@ export default (props) => {
       onClick={e => setActive(!active)}
       onPointerOver={e => setHover(true)}
       onPointerOut={e => setHover(false)}>
-      <sphereBufferGeometry attach="geometry" args={[0.1, 16, 16]} />
-      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+      <sphereBufferGeometry attach="geometry" args={[props.raduis, 16, 16]} />
+      {/* <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} /> */}
+
+      <meshStandardMaterial
+        attach="material"
+        color={props.color}
+        // emissive="white"
+        // emissiveIntensity={0.1}
+        // opacity={0.5}
+        // onBeforeCompile={(shader) => {
+        //   shader.vertexShader = shader.vertexShader
+        //     .replace( '#include <common>', colorParsChunk )
+        //     .replace( '#include <begin_vertex>', instanceColorChunk )
+
+        //   shader.fragmentShader = shader.fragmentShader
+        //     .replace( '#include <common>', fragmentParsChunk )
+        //     .replace( 'vec4 diffuseColor = vec4( diffuse, opacity );', colorChunk )
+        // }}
+      />
+
     </mesh>
   )
 }
--- assets/js/planet_disco/genres_view/index.jsx
+++ assets/js/planet_disco/genres_view/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from './box'
 import { OrbitControls } from 'drei'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import { scaleLinear, scalePow, scaleOrdinal } from 'd3-scale'
+import { max, min } from 'd3-array'
+import { schemeSpectral, interpolateSpectral } from 'd3'
 
 const GENRES = gql`{
   clusteredGenres {
@@ -16,22 +19,74 @@ const GENRES = gql`{
 export default () => {
 
   const { data } = useQuery(GENRES)
+
+  const radScale = scalePow()
+    .domain([0.000100067999999999, 0.006100348])
+    .range([0.09, 0.7, 1]);
+
+  const masterGenres = {
+    3712: 0,
+    1047: 1,
+    318: 2,
+    1214: 3,
+    865: 4,
+    986: 5,
+    3822: 6,
+    2193: 7,
+    669: 8,
+    2668: 9,
+    3134: 10,
+    1727: 11,
+    1861: 12,
+    1099: 13,
+    1252: 14,
+    2177: 15,
+    2368: 16,
+    3158: 17,
+    919: 18,
+    2224: 19,
+    1674: 20,
+    1135: 21,
+    1436: 22,
+    3488: 23,
+    2303: 24,
+    251: 25,
+    831: 26,
+    817: 27,
+    2913: 28,
+    768: 29,
+    1949: 30,
+    2019: 31,
+    721: 32,
+    267: 33,
+    1936: 34,
+    3618: 35,
+    2438: 36,
+    2452: 37
+}
+
+  const genreNorm = scaleLinear()
+    .domain([0, 37])
+    .range([0, 1]);
   
 
   return <scene>
     <OrbitControls />
-    <ambientLight intensity={0.3} />
-    <spotLight
+    <ambientLight intensity={1} />
+    {/* <spotLight
       castShadow
       intensity={1}
       angle={Math.PI / 8}
       position={[20, 10, 20]}
       shadow-mapSize-width={8192}
       shadow-mapSize-height={8192}
-    />
+    /> */}
     {data && data.clusteredGenres.map((genre, i) => {
       const { x, y, z } = genre.coord
-      return <Box key={i} position={[x, y, z]} />
+      const { name } = genre.name
+      const gRaduis = radScale(genre.pagerank)
+      const gcolor = interpolateSpectral(genreNorm(masterGenres[genre.masterGenreId]))
+      return <Box raduis={gRaduis} color={gcolor} key={i} position={[x, y, z]} />
     })}
   </scene>
 }
